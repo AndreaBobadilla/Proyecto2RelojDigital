9412fde4a776b037d2868574af21ec9e4e7a7be2
AndreaBobadilla <133414222+AndreaBobadilla@users.noreply.github.com>
2023-10-15 21:18:34 -0500

    UpdateRelojDigital
    UpdateRelojDigital

---
+++ Proyecto2RelojDigital/script.js
@@ -0,0 +1,20 @@
+function calcularTiempo(){
+    let tiempo = new Date();
+
+    let hora = tiempo.getHours();
+    let minuto = tiempo.getMinutes();
+    let segundo = tiempo.getSeconds();
+    let ampm = hora >= 12 ? "pm" : "am";
+
+
+    hora = hora < 10 ? "0" + hora : hora;
+    minuto = minuto < 10 ? "0" + minuto : minuto;
+    segundo = segundo < 10 ? "0" + segundo : segundo;
+
+
+    let pantallaReloj = hora + ":" + minuto + ":" + segundo + "-" + ampm;
+    let relojDigital = document.querySelector(".reloj");
+    relojDigital.innerHTML = pantallaReloj;
+}
+
+setInterval(calcularTiempo, 1000);
